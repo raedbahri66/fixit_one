3d6556ccb90baa772315ce4567c3b770a3260404
lenovo <lenovo@192.168.0.13>
2019-10-23 18:12:19 +0200

    5 raed bahri

--- src/main/java/GUI/Posteur_interfaceController.java
+++ src/main/java/GUI/Posteur_interfaceController.java
@@ -5,9 +5,16 @@
  */
 package GUI;
 
+import entites.Posteur;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import service.PosteurService;
 
 /**
  * FXML Controller class
@@ -16,12 +23,48 @@ import javafx.fxml.Initializable;
  */
 public class Posteur_interfaceController implements Initializable {
 
+    @FXML
+    private Label nomp_1;
+    @FXML
+    private Label telp_1;
+    @FXML
+    private Label emailp_1;
+    @FXML
+    private Label prenomp_1;
+    @FXML
+    private Label datep_1;
+    @FXML
+    private TextField tf_nom1;
+    @FXML
+    private TextField tf_prenom1;
+    @FXML
+    private TextField tef_email1;
+    @FXML
+    private TextField tf_tel1;
+    @FXML
+    private DatePicker df_date1;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        /////raed///
+        System.err.println(AcceuilController.cinlogin);
+        PosteurService p = new PosteurService();
+        Posteur p1= new Posteur();
+        p1 = p.getPosteurInfobyCin(AcceuilController.cinlogin);
+        nomp_1.setText(p1.getNom());
+        prenomp_1.setText(p1.getPrenom());
+        emailp_1.setText(p1.getEmail());
+        telp_1.setText(Integer.toString(p1.getTel()));
+         datep_1.setText(p1.getDate_naissance().toString());
+
     }    
+
+    @FXML
+    private void btn_modifprofil(ActionEvent event) {
+    }
     
 }
---
+++ src/main/java/service/ControleSaisie.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.regex.Pattern;
+import utils.ConnexionBD;
+
+/**
+ *
+ * @author lenovo
+ */
+public class ControleSaisie {
+    Connection c = ConnexionBD
+           .getInstanceConnexionBD()
+           .getConnection();
+    Statement ste;
+
+    public ControleSaisie() {
+        try {
+            ste = c.createStatement();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+    }
+    
+    public boolean emailisValid(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                            "[a-zA-Z0-9_+&*-]+)*@" +
+                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                            "A-Z]{2,7}$";
+                             
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+    public boolean cinisValid(String cin)
+    {
+        int taille = cin.length();
+        if( taille==8 && isInt(cin))
+        {
+            return true;
+        }
+        else return false;
+        
+    }
+    public boolean isInt(String s)
+    {
+        try
+        { int i = Integer.parseInt(s); return true; }
+
+        catch(NumberFormatException er)
+        { return false; }
+    }
+    public boolean isCintUsed(int cin1)
+    {
+        
+        String req1="select * from Posteur where cin="+cin1 ;   
+        String req2="select * from Jobeur where cin="+cin1;
+        String req3="select * from admin where cin="+cin1;   
+        boolean etat= false;
+        try {
+          ResultSet res=  ste.executeQuery(req1);
+          if (res.next()) { 
+etat=true;
+          }   
+          ResultSet res1=  ste.executeQuery(req2);
+          if (res1.next()) { 
+              etat= true;
+              }
+       ResultSet res2=  ste.executeQuery(req3);
+          if (res2.next()) { 
+              etat= true;
+              }
+              
+      } catch (SQLException ex) {
+          System.out.println(ex.getMessage());
+      } 
+             return etat; 
+  
+    }
+    
+}
