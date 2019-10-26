c329ab34694987ab00b6c1af15b026d407a0d5c1
Wael <Wael@DESKTOP-8VF4QGK>
2019-10-26 04:33:31 +0100

    gestion produit combobox filter(filtrage liste)

--- src/main/java/GUI/Posteur_interfaceController.java
+++ src/main/java/GUI/Posteur_interfaceController.java
@@ -458,7 +458,7 @@ public class Posteur_interfaceController implements Initializable {
                  categorie_produit.getItems().addAll("Jardinage","Electricité","Batimmant","Informatique","Electromenager");
                  categorie_produit2.getItems().addAll("Jardinage","Electricité","Batimmant","Informatique","Electromenager");
                  categorie_produit3.getItems().addAll("non_vendu","vendu");
-                 combobox_filter.getItems().addAll("","Jardinage","Electricité","Batimmant","Informatique","Electromenager");
+                 combobox_filter.getItems().addAll("Tous()","Jardinage","Electricité","Batimmant","Informatique","Electromenager");
     
     }    
 
